fc674b9d0cf0b940ee2495226f1fe657b92c15b5
Yelshat Toyekin <y.toekin@galamat.biz>
2022-12-27 15:05:56 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+import librosa
+from loguru import logger
+import ndjson
+
+
+TARGET_DIRS = ("Audios", "Transcripts")
+SPEAKER_DIR = "M1_Iseke"
+OUT_DIR = "datasets/manifest/"
+logger.add("logs/error.log", format="{time}{level}{message}")
+audio_path = os.path.join(SPEAKER_DIR, TARGET_DIRS[0])
+text_path = os.path.join(SPEAKER_DIR, TARGET_DIRS[1])
+
+
+def get_transcribe(audio_filepath):
+    filename = os.path.basename(audio_filepath).replace(".wav", ".txt")
+    filepath = os.path.join(text_path, filename)
+    if os.path.exists(filepath):
+        with open(filepath, "r") as file:
+            text = file.readline()
+        return text
+    else:
+        logger.info(f"file not founded: {filename}")
+        return None
+
+
+def get_duration(audio_filepath):
+    try:
+        duration = librosa.core.get_duration(filename=audio_filepath)
+        return duration
+    except Exception as ex:
+        logger.info(f"audio file broken: {audio_filepath}")
+        logger.error(ex)
+        return None
+
+
+# create Dataframe
+audio_filepaths = pd.Series(
+    os.path.join(audio_path, file) for file in os.listdir(audio_path)
+)
+df = pd.DataFrame(audio_filepaths, columns=["audio_filepath"])
+df["text"] = df.apply(lambda x: get_transcribe(x["audio_filepath"]), axis=1)
+df["duration"] = df.apply(lambda x: get_duration(x["audio_filepath"]), axis=1)
+
+# save result
+out_csv = os.path.join(OUT_DIR, SPEAKER_DIR+".csv")
+out_json = os.path.join(OUT_DIR, SPEAKER_DIR+".json")
+df.to_csv(out_csv, index=False)
+with open(out_json, "w") as file:
+    dict_records = df.to_dict(orient="records")
+    ndjson.dump(dict_records, file, ensure_ascii=False)
